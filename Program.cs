103e577d4f72da3c9ad492a32d20b11338cb2978
dhanashripatil205 <dhanashripatil205@gmail.com>
2022-12-29 00:32:44 +0530

    Mehod calling

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@
             Console.WriteLine("******** WELCOME TO LOGICAL PROGRAMMING ");
 
 
-            ReverseNumber.CheckReverseNumber();
+            QuotientRemainder.CalculateQuotientAndRemainder();
 
 
         }
