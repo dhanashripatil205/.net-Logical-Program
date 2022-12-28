6300ee0ce53d82fbf717ab772f06fa6326b619bf
dhanashripatil205 <dhanashripatil205@gmail.com>
2022-12-29 00:37:26 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@
             Console.WriteLine("******** WELCOME TO LOGICAL PROGRAMMING ");
 
 
-            QuotientRemainder.CalculateQuotientAndRemainder();
+            PrimeNo.CheckPrimeNumber();
 
 
         }
