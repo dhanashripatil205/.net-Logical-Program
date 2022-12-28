779904a5ce7e049b03dc28e9467044efa5d158ab
dhanashripatil205 <dhanashripatil205@gmail.com>
2022-12-29 00:02:27 +0530

    Method calling

--- Program.cs
+++ Program.cs
@@ -8,21 +8,8 @@
 
             Console.WriteLine("******** WELCOME TO LOGICAL PROGRAMMING ");
 
-            //PrimeFactor.PrimeFactorization();
-            //Harmonic.Harmonicnumber();
-            //PrimeFactor.PrimeFactorization();
-            //Factorial.CalculateFactorial();
-            // EvenOdd.CheckEvenOdd();
-            //SwapTwoNum.TwoNumSwap();
-            //LargestNumber.FindLargestNum();
-            //VowelOrConstant.CheckVowelConstant();
-            //QuotientRemainder.CalculateQuotientAndRemainder();
-            //Fibonacci.FindFibonacci();
-            //PerfectNumber.CheckPerfectNumber();
-            //PrimeNo.CheckPrimeNumber();
-            //ReverseNumber.CheckReverseNumber();
-            //Coupon.CheckCoupon();
-            //StopWatch.StartStopwatch();
+            
+            StopWatch.StartStopwatch();
 
 
         }
