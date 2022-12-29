1b1a8d11c6e371da7c9f350a19f29f931afa3491
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2022-12-29 16:43:01 +0530

    Update PerfectNumber.cs

--- PerfectNumber.cs
+++ PerfectNumber.cs
@@ -40,3 +40,63 @@ namespace LogicalProgram
         }
     }
 }
+
+//Using Nested for Loop solution
+
+/*int i, sum, mn, mx;
+            Console.WriteLine("Find the Perfect Number within the given range or number: ");
+            Console.WriteLine("Input the String range or number: ");
+             mn = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the ending range or number: ");
+             mx = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The perfect number witin the given range");
+
+            for(int n = mn; n <= mx; n++)
+            {
+                
+                sum = 0;
+                for (i=1; i<n; i++)               
+                {
+                    if (n % i == 0)
+
+                        sum = sum + i;
+                    
+                }   
+                    if(sum == n)
+                    
+                        Console.WriteLine(" " + n);
+           }*/
+
+//using while loop solution
+/*
+int i, sum, mn, mx;
+            Console.WriteLine("Find the Perfect Number within the given range or number: ");
+            Console.WriteLine("Input the String range or number: ");
+             mn = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the ending range or number: ");
+             mx = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The perfect number witin the given range");
+
+            for(int n = mn; n <= mx; n++)
+            {
+                
+                sum = 0;
+                i=1; 
+                while(i<n)               
+                {
+                    if (n % i == 0)
+
+                        sum = sum + i;
+                    i++;
+                }   
+                    if(sum == n)
+                    
+                        Console.WriteLine(" " + n);
+
+                    
+                
+            } */
