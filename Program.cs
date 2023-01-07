809c8513f9972714c614ef1cbc1a0a0e1342ff06
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-07 16:32:10 +0530

    Method calling

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace LogicalProgram
 
             Console.WriteLine("******** WELCOME TO N UNIT Testing PROGRAMMING ");
 
-            Week.DayOfWeek();
+            MonthlyPayment.Payment();
 
 
         }
